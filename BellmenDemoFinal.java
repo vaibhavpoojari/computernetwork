4ba4f9fcd389e0191b482c1c86f6afb8c5f6b298
VAIBHAVA G <vaibhavg8121@gmail.com>
2025-09-19 16:11:40 +0530

    Update BellmenDemoFinal.java

--- BellmenDemoFinal.java
+++ BellmenDemoFinal.java
@@ -1,65 +1,70 @@
-
-package SMVITM.com;
+package smvitm.cnlab;
 import java.util.Scanner;
-public class BellmenDemoFinal {
- static Scanner in=new Scanner(System.in);
-    public static void main(String[] args) {
-        int V,E=1,checkNegative=0;
-        int W[][]=new int[20][20];
-        int edge[][]=new int [50][2];
-        System.out.println("Enetr the no of Vertices");
-        V=in.nextInt();
-        System.out.println("Enetr the weight matrix");
-        for(int i=1;i<=V;i++)
-            for(int j=1;j<=V;j++){
-                W[i][j]=in.nextInt();
-                if(W[i][j]!=0)
-                {
-                    edge[E][0]=i;
-                    edge[E++][1]=j;
-                }
-            }
-        checkNegative=bellmenFord(W,V,E,edge);
-        if(checkNegative==1)
-                    System.out.println("\n No negative Weight cycle\n");
-        else
-                    System.out.println("\n Negative weight cycle exists\n");
-    }
-    public static int bellmenFord(int W[][],int V,int E,int edge[][])
-    {
-        int u,v = 0,S,flag=1;
-        int distance[]=new int [20];
-        int parent[]=new int [20];
-        for(int i=1;i<=V;i++)
-        {
-            distance[i]=999;
-            parent[i]=-1;
+public class Bellmandemofinal {
+  static Scanner in = new Scanner(System.in);
+  public static void main(String[] args) {
+    int v, e = 0, checkNegative;
+    int[][] w;
+    int[][] edge;
+    System.out.print("Enter number of vertices: ");
+    v = in.nextInt();
+    w = new int[v][v];  
+    edge = new int[50][2];  
+    System.out.println("Enter the weight matrix (0 for no edge):");
+    for (int i = 0; i < v; i++) {
+      for (int j = 0; j < v; j++) {
+        w[i][j] = in.nextInt();
+        if (w[i][j] != 0) {
+          edge[e][0] = i;
+          edge[e][1] = j;
+          e++;
         }
-                System.out.println("Enter the source vertex");
-                S=in.nextInt();
-                distance[S]=0;
-                for(int i=1;i<=V-1;i++){
-                    for(int K=1;K<=E;K++){
-                    u=edge[K][0];
-                    v=edge[K][1];
-                    if(distance[u]+W[u][v]<distance[v]){
-                        distance[V]=distance[u]+W[u][v];
-                        parent[v]=u;
-                    }
-                }
-                }
-    for( int K=1;K<=E;K++)
-    {
-        u=edge[K][0];
-        V=edge[K][1];
-        if(distance[u]+W[u][v]<distance[v])
-            flag=0;
+      }
     }
-        if(flag==1)
-            for(int i=1;i<=V;i++)
-                System.out.println("Vertex "+i+"->cost="+distance[i]+"parent="+(parent[i]));
-        return flag;
+    checkNegative = bellmanFord(w, v, e, edge);
+    if (checkNegative == 1)
+      System.out.println("\nNo negative weight cycle");
+    else
+      System.out.println("\nNegative weight cycle exists");
+  }
+  public static int bellmanFord(int[][] w, int v, int e, int[][] edge) {
+    int u, v1, s, flag = 1;
+    int[] distance = new int[v];
+    int[] parent = new int[v];
+    for (int i = 0; i < v; i++) {
+      distance[i] = Integer.MAX_VALUE;
+      parent[i] = -1;
     }
+    System.out.print("Enter the source vertex: ");
+    s = in.nextInt();
+    distance[s] = 0;
+    for (int i = 1; i <= v - 1; i++) {
+      for (int k = 0; k < e; k++) {
+        u = edge[k][0];
+        v1 = edge[k][1];
+        if (distance[u] != Integer.MAX_VALUE && distance[u] + w[u][v1] < distance[v1]) {
+          distance[v1] = distance[u] + w[u][v1];
+          parent[v1] = u;
+        }
+      }
+    }
+    for (int k = 0; k < e; k++) {
+      u = edge[k][0];
+      v1 = edge[k][1];
+      if (distance[u] != Integer.MAX_VALUE && distance[u] + w[u][v1] < distance[v1]) {
+        flag = 0;
+      }
+    }
+    if (flag == 1) {
+      for (int i = 0; i < v; i++) {
+        if (distance[i] == Integer.MAX_VALUE) {
+          System.out.println("Vertex " + i + " -> No path");
+        } else {
+          System.out.println("Vertex " + i + " -> Cost = " + distance[i] + ", Parent = " + parent[i]);
+        }
+      }
+    }
+    return flag;
+  }
 }
 
-                    
